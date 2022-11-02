70f4aaa43d56652ffebf204ccda58df46e0a2aba
ananddhobi <55586074+ananddhobi@users.noreply.github.com>
2022-11-03 03:49:41 +0530

    Update portforward.sh

--- portforward.sh
+++ portforward.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+#set -x
 ###################################################################
 #Script Name	:   portforward.sh                                                                                           
 #Description	:   forward port from local machine to remote machine                                                                                                                                                                       
@@ -9,15 +9,14 @@
 
 TOTAL_OPEN_PORT=`ps -aux| grep -i "ssh -o StrictHostKeyChecking=no -f -N -R" | grep -v grep |wc -l`
 CURRENT_PORT=`ps -aux|grep "ssh -o StrictHostKeyChecking=no -f -N -R" | grep -v grep | awk {'print $17'} | cut -d ":" -f1`
-COUNT=4
+COUNT=3
 
-check_passh_status(){
-if [ -e /usr/bin/passh ]
+check_sshpass_status(){
+if [ -e /usr/bin/sshpass ]
 then
-open_port
+check_if_port_already_running_on_remote_server
 else
-install_passh
-open_port
+echo "sshpass not installed,please install sshpass"
 fi
 }
 
@@ -25,16 +24,24 @@ fi
 check_if_port_already_running_on_local_machine(){
 if (( $TOTAL_OPEN_PORT>=$COUNT ))
 then
-echo -e "$TOTAL_OPEN_PORT port already exposed on remote server from this machine,can not open another.\nExposed port\n$CURRENT_PORT"
+echo -e "$TOTAL_OPEN_PORT port already exposed on remote server from this machine,can not open another.\nExposed port/ports\n$CURRENT_PORT"
 exit 0;
 else
 echo "port $EXPOSE_PORT not running on local machine"
-check_passh_status
+open_port
 fi
 }
 
 check_if_port_already_running_on_remote_server(){
-passh -p $PASSWORD ssh $SSHUSER@$HOST "netstat -tulpn | grep $EXPOSE_PORT" > /dev/null 2>&1
+if (( $TOTAL_OPEN_PORT>=$COUNT ))
+then
+echo -e "$TOTAL_OPEN_PORT port already exposed on remote server from this machine,can not open another.\nExposed port/ports\n$CURRENT_PORT"
+exit 0;
+else
+
+
+	
+sshpass -p $PASSWORD ssh $SSHUSER@$HOST "netstat -tulpn | grep $EXPOSE_PORT" > /dev/null 2>&1
 if [ $? -eq 0 ]
 then
         echo "port $EXPOSE_PORT already running on remote server $HOST , Use another port"
@@ -43,18 +50,21 @@ else
         echo "port $EXPOSE_PORT not running on remote server"
 check_if_port_already_running_on_local_machine
 fi
+
+
+fi
 }
  
  
 open_port(){
 echo "opening port $EXPOSE_PORT"
-passh -p $PASSWORD ssh -o StrictHostKeyChecking=no -f -N -R $EXPOSE_PORT:127.0.0.1:22 $SSHUSER@$HOST > /dev/null 2>&1
+sshpass -p $PASSWORD ssh -o StrictHostKeyChecking=no -f -N -R $EXPOSE_PORT:127.0.0.1:22 $SSHUSER@$HOST > /dev/null 2>&1
 if [ $? == 0 ]
 then
 local CURRENT_PORT=`ps -aux |grep "ssh -o StrictHostKeyChecking=no -f -N -R" | grep -v grep | awk {'print $17'} | cut -d ":" -f1`
-echo "current running port $CURRENT_PORT"
+echo -e "current running port(s)\n$CURRENT_PORT"
 local test=`ps -aux |grep "ssh -o StrictHostKeyChecking=no -f -N -R" | grep -v grep | awk {'print $17'} | cut -d ":" -f1 | tac`
-echo "latest open port $test"
+echo -e "latest open port(s)\n$test"
 local CURRENT_TOTAL=`ps -aux| grep -i "ssh -o StrictHostKeyChecking=no -f -N -R" | grep -v grep |wc -l`
 
  
@@ -66,15 +76,12 @@ fi
 
 if (( $CURRENT_TOTAL>=1 ))
 then
-echo "going inside loop"
 for ports in $test
 do
-echo "inside loop"
-echo $ports
 if [ $EXPOSE_PORT -eq $ports ]
 then
 exit
-#open_port
+
 else
 open_port
 fi
@@ -87,22 +94,10 @@ echo "Provided credientials wrong"
 exit
 fi
 }
- 
-
-install_passh(){
-echo "installing passh"
-git clone https://github.com/clarkwang/passh.git > /dev/null 2>&1
-cd passh
-cc -o passh passh.c
-mv passh /usr/bin/
-cd ..
-rm -rf passh
-echo "passh installation complete"
-}
 
 
 want_to_exit_or_again_open_another_port(){
-echo "press y/Y to exit,any other key for again"
+echo "press y/Y to exit,any other key for again or simply press Enter"
 read ans
 if [[ "$ans" =~ ^([yY][eE][sS]|[yY])$ ]]
 then
@@ -110,21 +105,21 @@ exit
 else
 user_input
 check_if_port_already_running_on_remote_server
-check_passh_status
+#check_sshpass_status
 fi
 }
 
 
 user_input(){
+echo "Enter remote server IP"
+read HOST
 echo "Enter username"
 read SSHUSER
-echo "Enter server IP"
-read HOST
 echo "Enter port for expose"
 read EXPOSE_PORT
 echo "Enter password"
-read PASSWORD
+read -s  PASSWORD
 }
 
 user_input
-check_if_port_already_running_on_remote_server
+check_sshpass_status
